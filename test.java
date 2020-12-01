c30ab64994886939b93ca62ddf2eb719b66a745a
nadhifalR <nadhifal01@gmail.com>
2020-12-01 23:45:32 +0700

    lambdaNS & lambda

---
+++ test.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class test{
+    public static void main(String args[]){
+        // Lambda lambda = new Lambda(1,1);
+        Lambda lambda = new Lambda();
+
+        System.out.println("res :" + lambda.count(28,27,1,29));
+
+        
+        // key Key = new key(29);
+        // Key.run();
+
+        // System.out.println("p :" + Key.get());
+        /*
+        isSame = true -> lambda(x,y)
+        isSame = false -> lambdaNS(x,y,x2,y2);
+        */
+
+        /*
+        Y1 = qa
+        Y2 = (p1, p2) + q(ra)
+        */
+
+        /*
+        (p1, p2) = y2 - r(y1)
+        */
+    }
+}
