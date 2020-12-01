0ed16622f1ce7bdc4915db69747b053acaf15e48
nadhifalR <nadhifal01@gmail.com>
2020-12-02 00:48:24 +0700

    lambda fix

--- LambdaNS.java
+++ LambdaNS.java
@@ -10,17 +10,43 @@ public class LambdaNS {
     
     public double count(double x, double y, double x2, double y2, double p){
         this.p = p;
-        res = ((y2 - y) * modInverse((x2 - x), p)) % p;
+        // res = ((y2 - y) * modInverse((x2 - x), p)) % p;
+
+        res = ((y2 - y) * modInverse((x2 - x), p));
+        if (res>=0) {
+            res = res % p;    
+        }
+        else{
+            res = p + res;
+        }
+
         return res;
     }
 
     public double getX(double x, double x2){
-        x3 = ((Math.pow(this.res, 2)) - x - x2) % p;
+        //x3 = ((Math.pow(this.res, 2)) - x - x2) % p;
+        
+        x3 = ((Math.pow(this.res, 2)) - x - x2);
+        if (x3>=0) {
+            x3 = x3 % p;    
+        }
+        else{
+            x3 = p + x3;
+        }
+
         return x3;
     }
 
     public double getY(double x, double y){
-        y3 = (this.res*(x - x3) - y) % p;
+        //y3 = (this.res*(x - x3) - y) % p;
+
+        y3 = ((this.res*(x - x3)) - y);
+        if (y3>=0) {
+            y3 = y3 % p;    
+        }
+        else{
+            y3 = p + y3;
+        }
         return y3;
     }
 
