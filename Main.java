6dd02549207ac3bd90a75feab7b0a9b32ff5a377
kdknive <kevindknive@gmail.com>
2020-12-01 20:17:12 +0700

    papuy

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String args[]){
         Lambda lambda = new Lambda();
@@ -6,11 +8,39 @@ public class Main{
         isSame = true -> lambda(x,y)
         isSame = false -> lambdaNS(x,y,x2,y2);
         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan p =   ");
+        int p = scanner.nextInt();
+        System.out.print("Masukkan a =   ");
+        int a = scanner.nextInt();
+        System.out.print("Masukkan b =   ");
+        int b = scanner.nextInt();
+        System.out.print("Masukkan q =   ");
+        int q = scanner.nextInt();
+        System.out.print("Masukkan r =   ");
+        int r = scanner.nextInt();
+        System.out.println("Masukkan Plaintext");
+        System.out.print("P1 = ");
+        int P1 = scanner.nextInt();
+        System.out.print("P2 = ");
+        int P2 = scanner.nextInt();
+        System.out.println("Masukkan Fungsi Pembangkit");
+        System.out.print("x1 = ");
+        int x1 = scanner.nextInt();
+        System.out.print("y1 = ");
+        int y1 = scanner.nextInt();
 
+        int array1[][] = new int[2][3];
+        array1[0][0] = x1;
+        array1[0][1] = y1;
         /*
         Y1 = qa
         Y2 = (p1, p2) + q(ra)
         */
+        
+        if q = 2 {
+            
+        }
 
         /*
         (p1, p2) = y2 - r(y1)
