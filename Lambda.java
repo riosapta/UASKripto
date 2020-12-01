13d2a379c8204728071e6db9744eb8e3c3c3c08c
nadhifalR <nadhifal01@gmail.com>
2020-12-02 00:52:36 +0700

    lambda fix

--- Lambda.java
+++ Lambda.java
@@ -12,18 +12,39 @@ public class Lambda {
 
     public double count(double x, double y, double a, double p){
         this.p= p;
-        res = (((3*(Math.pow(x, 2)) + a))*modInverse(2*y, p)) % p;
+        // res = (((3*(Math.pow(x, 2)) + a))*modInverse(2*y, p)) % p;
+        res = (((3*(Math.pow(x, 2)) + a))*modInverse(2*y, p));
+        if (res>=0) {
+            res = res % p;    
+        }
+        else{
+            res = p + res;
+        }
         return res;
     }
 
     public double getX(double x, double x2){
-        x3 = ((Math.pow(this.res, 2)) - x - x2) % p;
+        // x3 = ((Math.pow(this.res, 2)) - x - x2) % p;
+        x3 = ((Math.pow(this.res, 2)) - x - x2);
+        if (x3>=0) {
+            x3 = x3 % p;    
+        }
+        else{
+            x3 = p + x3;
+        }
         System.out.println(x3);
         return x3;
     }
 
     public double getY(double x, double y){
-        y3 = (this.res*(x - x3) - y) % p;
+        // y3 = (this.res*(x - x3) - y) % p;
+        y3 = (this.res*(x - x3) - y);
+        if (y3>=0) {
+            y3 = y3 % p;    
+        }
+        else{
+            y3 = p + y3;
+        }
         System.out.println(y3);
         return y3;
     }
