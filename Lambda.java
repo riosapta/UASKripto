0c86444cea913f4de329b6e64f23d309dc5536af
nadhifalR <nadhifal01@gmail.com>
2020-12-01 22:13:01 +0700

    lambda mod p

--- Lambda.java
+++ Lambda.java
@@ -1,24 +1,26 @@
 import java.lang.Math; 
 public class Lambda {
     double res;
+    double p;
     double x3;
     double y3;
     public Lambda(){
         this.res = 0;
     }
 
-    public double count(double x, double y, double a){
-        res = ((Math.pow(x, 3)) + a)/(2 * y);
+    public double count(double x, double y, double a, double p){
+        this.p= p;
+        res = (((Math.pow(x, 3)) + a)/(2 * y)) % p;
         return res;
     }
 
     public double getX(double x, double x2){
-        x3 = (Math.pow(this.res, 2)) - x - x2;
+        x3 = ((Math.pow(this.res, 2)) - x - x2) % p;
         return x3;
     }
 
     public double getY(double x, double y){
-        y3 = this.res*(x - x3) - y;
+        y3 = (this.res*(x - x3) - y) % p;
         return y3;
     }
 }
--- LambdaNS.java
+++ LambdaNS.java
@@ -1,5 +1,6 @@
 public class LambdaNS {
     double res;
+    double p;
     double x3;
     double y3;
 
@@ -7,18 +8,19 @@ public class LambdaNS {
         this.res=0;
     }
     
-    public double count(double x, double y, double x2, double y2){
-        res = (y2 - y) / (x2 - x);
+    public double count(double x, double y, double x2, double y2, double p){
+        this.p = p;
+        res = ((y2 - y) / (x2 - x)) % p;
         return res;
     }
 
     public double getX(double x, double x2){
-        x3 = (Math.pow(this.res, 2)) - x - x2;
+        x3 = ((Math.pow(this.res, 2)) - x - x2) % p;
         return x3;
     }
 
     public double getY(double x, double y){
-        y3 = this.res*(x - x3) - y;
+        y3 = (this.res*(x - x3) - y) % p;
         return y3;
     }
 }
